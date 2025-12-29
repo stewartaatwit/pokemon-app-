b247ac01c575a66306ae06336799aff55d192888
Atalya Stewart <stewarta6@wit.edu>
2025-12-29 18:52:26 -0500

    created html structure for pokemon flip cards and is now creating a function to create mutliple of them

---
+++ proj.js
@@ -0,0 +1,13 @@
+
+//creates a function that creates a pokemon card with the image and its stats and characteristics
+function createPokemonCard(pokemon){
+    //table variable holds reference to tbody in html
+    //need this table for the structure of rows and columns
+    const table = document.getElementById("pokemonContainer");
+    //holds reference to rows in html
+    const rows= document.getElementById("pokemonRow");
+    //holds reference to cells in html
+    const cell= document.getElementById("pokemonCell");
+
+    
+}
